d1edd6c40cd73b4953901f8c113bc39023ff4940
unknown <help0304@naver.com>
2023-08-06 17:58:01 +0900

    "Auto Commit Test"

--- commit_reservation/src/services/timer.rs
+++ commit_reservation/src/services/timer.rs
@@ -1,13 +1,39 @@
-use crate::services::handlers::time_handler::TimeHandler;
+use crate::{services::handlers::{time_handler::TimeHandler, command_handler::CommandHandler}, domain::command::GitCommand};
 use std::thread::sleep;
 
 #[warn(while_true)]      // TODO Add Exit Logic
 pub fn run(){
     while true{
-        let time_handler = TimeHandler::new();
-        sleep(time_handler.get_interval());
+        let time_handler1 = TimeHandler::new();
+        let time_handler2 = TimeHandler::new();
+        println!("Start Timer");
+        sleep(time_handler1.get_interval());
+        println!("Start Auto Commit at {:?}", time_handler2.get_checking_time());
+        auto_commit();
+        println!("Successed Auto Commit");
         // TODO Check if it needs to commit
-
+        // if need_commit(){
+        //     auto_commit();
+        // }
     }
 }
 
+fn need_commit() -> bool{ // TODO Use MessageBus
+        let command_handler_tester: CommandHandler = CommandHandler;
+        let output = command_handler_tester.execute(Box::new(GitCommand::GetTodaysCommit));
+        if output.is_empty(){
+            return true
+        }
+        false
+    }
+
+fn auto_commit(){
+    let command_handler_tester: CommandHandler = CommandHandler;
+    let output = command_handler_tester.execute(Box::new(GitCommand::Pull));
+    let command_handler_tester: CommandHandler = CommandHandler;
+    let output = command_handler_tester.execute(Box::new(GitCommand::Add { path: "*".to_string() }));
+    let command_handler_tester: CommandHandler = CommandHandler;
+    let output = command_handler_tester.execute(Box::new(GitCommand::Commit { message: "Auto Commit Test".to_string() }));
+    let command_handler_tester: CommandHandler = CommandHandler;
+    let output = command_handler_tester.execute(Box::new(GitCommand::Push));
+}
